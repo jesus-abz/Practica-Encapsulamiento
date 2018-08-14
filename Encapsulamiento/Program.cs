40377f4cc17ab2aea07e2377c4ebb695926718f2
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 08:35:31 -0700

    Agregar archivos de proyecto.

---
+++ Encapsulamiento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamiento
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string Email { get; set; }
+    }
+}
